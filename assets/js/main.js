e59b9f28a4eea98c465fc7206260eea15c321de5
Issatarraf07 <issatarraf07@gmail.com>
2024-08-05 23:35:37 -0800

    new update

---
+++ assets/js/main.js
@@ -0,0 +1,74 @@
+/*=============== SHOW MENU ===============*/
+const showMenu = (toggleId, navId) =>{
+   const toggle = document.getElementById(toggleId),
+         nav = document.getElementById(navId)
+
+   toggle.addEventListener('click', () =>{
+       // Add show-menu class to nav menu
+       nav.classList.toggle('show-menu0')
+       // Add show-icon to show and hide menu icon
+       toggle.classList.toggle('show-icon0')
+   })
+}
+
+showMenu('nav-toggle0','nav-menu0')
+
+/*=============== SHOW DROPDOWN MENU ===============*/
+const dropdownItems = document.querySelectorAll('.dropdown__item0')
+
+// 1. Select each dropdown item
+dropdownItems.forEach((item) =>{
+    const dropdownButton = item.querySelector('.dropdown__button0') 
+
+    // 2. Select each button click
+    dropdownButton.addEventListener('click', () =>{
+        // 7. Select the current show-dropdown class
+        const showDropdown = document.querySelector('.show-dropdown0')
+        
+        // 5. Call the toggleItem function
+        toggleItem(item)
+
+        // 8. Remove the show-dropdown class from other items
+        if(showDropdown && showDropdown!== item){
+            toggleItem(showDropdown)
+        }
+    })
+})
+
+// 3. Create a function to display the dropdown
+const toggleItem = (item) =>{
+    // 3.1. Select each dropdown content
+    const dropdownContainer = item.querySelector('.dropdown__container0')
+
+    // 6. If the same item contains the show-dropdown class, remove
+    if(item.classList.contains('show-dropdown0')){
+        dropdownContainer.removeAttribute('style')
+        item.classList.remove('show-dropdown0')
+    } else{
+        // 4. Add the maximum height to the dropdown content and add the show-dropdown class
+        dropdownContainer.style.height = dropdownContainer.scrollHeight + 'px'
+        item.classList.add('show-dropdown0')
+    }
+}
+
+/*=============== DELETE DROPDOWN STYLES ===============*/
+const mediaQuery = matchMedia('(min-width: 1118px)'),
+      dropdownContainer = document.querySelectorAll('.dropdown__container0')
+
+// Function to remove dropdown styles in mobile mode when browser resizes
+const removeStyle = () =>{
+    // Validate if the media query reaches 1118px
+    if(mediaQuery.matches){
+        // Remove the dropdown container height style
+        dropdownContainer.forEach((e) =>{
+            e.removeAttribute('style')
+        })
+
+        // Remove the show-dropdown class from dropdown item
+        dropdownItems.forEach((e) =>{
+            e.classList.remove('show-dropdown0')
+        })
+    }
+}
+
+addEventListener('resize', removeStyle)
